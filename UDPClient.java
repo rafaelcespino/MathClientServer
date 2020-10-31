64c56e8aa439b81adc2d53a4d0f4c1bfbcfb150d
Rafael <rafaelespino200@gmail.com>
2020-10-31 17:19:46 -0500

    Added full functionality to send and receive equations/solutions as well as tracking user logs.

--- UDPClient.java
+++ UDPClient.java
@@ -1,5 +1,7 @@
 import java.io.*; 
 import java.net.*; 
+import java.time.Duration;
+import java.time.Instant;
   
 class UDPClient { 
     public static void main(String args[]) throws Exception 
@@ -7,6 +9,9 @@ class UDPClient {
       //creates buffered reader for user input and UDP socket for the datagram
       BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in)); 
       DatagramSocket clientSocket = new DatagramSocket(); 
+
+      //create the start time for connection duration
+      Instant start = Instant.now();
   
       //stores client IP Address in IPAddress variable
       InetAddress IPAddress = InetAddress.getByName("127.0.0.1"); 
@@ -18,8 +23,25 @@ class UDPClient {
       String name = inFromUser.readLine();
       System.out.println("Name: " + name);
       //gets String equation from user and sends as Bytes
+
+      while(true){
+      //get equation input from user as string
       String equation = inFromUser.readLine(); 
-      sendData = equation.getBytes();         
+
+      //if the user types exit, send dataCSV with the total connection time before closing the socket and ending the program
+      if(equation.equals("Exit") || equation.equals("exit")){
+        Instant end = Instant.now();
+        Duration connectionTime = Duration.between(start, end);
+        String dataCSV = name+ "," + "Exit" + "," + connectionTime.toSeconds();
+        sendData = dataCSV.getBytes();         
+        DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+        clientSocket.close(); 
+        break;
+      }
+
+      //puts all data together as a CSV in order of name, equation, and time elapsed
+      String dataCSV = name+ "," + equation + ",0"; //time elapsed is zero since the connection hasn't been ended 
+      sendData = dataCSV.getBytes();         
       //Datagram contains the equation, length, IP address, and port number
       DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
     	clientSocket.send(sendPacket); 
@@ -29,9 +51,12 @@ class UDPClient {
     	  
     	clientSocket.receive(receivePacket); 
     	  
-    	String answer = new String(receivePacket.getData()); 
+    	String answer = new String(receivePacket.getData(), 0, receivePacket.getLength()); 
     	  
-    	System.out.println("FROM SERVER:" + answer); 
-    	clientSocket.close(); 
+      System.out.println("FROM SERVER:" + answer); 
+      
+      }
+
+      
   } 
 }
--- UDPServer.java
+++ UDPServer.java
@@ -1,12 +1,42 @@
 import java.io.*; 
 import java.net.*; 
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.Date;
+import java.util.ArrayList;
   
 class UDPServer { 
+
+  static class User{
+    private String name;
+    private String secsAttached;
+    private Date timeConnected;
+  
+    User(String name, String secs){
+      this.name = name;
+      this.secsAttached = secs;
+      timeConnected = new Date();
+    }
+  
+    public String getName(){
+      return this.name;
+    }
+  
+    public String getSecs(){
+      return this.secsAttached;
+    }
+  
+    public Date getTime(){
+      return this.timeConnected;
+    }
+  }
+
   public static void main(String args[]) throws Exception 
     { 
   
       DatagramSocket serverSocket = new DatagramSocket(9876); 
-  
+      ArrayList<User> userLogs = new ArrayList<User>();
       byte[] receiveData = new byte[1024]; 
       byte[] sendData  = new byte[1024]; 
   
@@ -16,17 +46,43 @@ class UDPServer {
           DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length); 
            serverSocket.receive(receivePacket); 
 
-           String sentence = new String(receivePacket.getData()); 
-   
+           //converts message into string from byte array 
+           String receivedMessage = new String(receivePacket.getData(), 0, receivePacket.getLength()); 
+
+           //splits comma separated string into array 
+           String[] separatedMessage = receivedMessage.split(",");
+           String name = separatedMessage[0]; //first item in array is the name of the sender
+           String equation = separatedMessage[1]; //second item in array is the equation to be evaluated
+           String secsAttached = separatedMessage[2];
+
            InetAddress IPAddress = receivePacket.getAddress(); 
-   
            int port = receivePacket.getPort(); 
    
-           String capitalizedSentence = sentence.toUpperCase(); 
+           //Script Engine allows javascript eval function to evaluate the string equation automatically
+           ScriptEngineManager mgr = new ScriptEngineManager();
+           ScriptEngine engine = mgr.getEngineByName("JavaScript");
 
-           sendData = capitalizedSentence.getBytes(); 
+           //logs the user on exit by creating a user object and storing it in the user log arraylist
+           if(equation.contains("exit"))
+           {
+              
+              User newUser = new User(name, secsAttached);
+           }
+           else {
+             try {
+              System.out.println(name);
+              String result = engine.eval(equation).toString();
+              sendData = result.getBytes(); 
+             } catch (ScriptException e) {
+               System.out.println("Exception " + e);
+               String exceptionMessage = "Error: Please enter a valid mathematical expression";
+               sendData =  exceptionMessage.getBytes();
+             }
+           }
    
-           DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port); 
+           DatagramPacket sendPacket = 
+              new DatagramPacket(sendData, sendData.length, IPAddress, 
+                                port); 
    
            serverSocket.send(sendPacket); 
          } 
